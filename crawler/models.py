e3a69fa1c407c8e48dcf50c22a01c0e1ed71c641
Aknifee <janusz.j.michal@gmail.com>
2020-01-27 12:23:15 +0100

    modified user model

--- crawler/models.py
+++ crawler/models.py
@@ -4,9 +4,10 @@ from django.db import models
 
 # Create your models here.
 
-class Adress(models.Model):
+class UserInfo(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     city = models.CharField(max_length=128, blank=True)
     street = models.CharField(max_length=256, blank=True)
     house_nr = models.IntegerField(blank=True)
     flat_nr = models.IntegerField(blank=True)
+    phone_number = models.IntegerField(blank=True, default=None)
